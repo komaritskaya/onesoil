e9b87910d9b489cc861c700c2809ac81690b34f0
komaritskaya <komaritskaya@gmail.com>
2018-08-23 12:14:38 +0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -38,11 +38,13 @@ const buffer = require('vinyl-buffer');
 const merge = require('merge-stream');
 const wait = require('gulp-wait');
 const htmlbeautify = require('gulp-html-beautify');
+const babel = require('gulp-babel');
 
 // Перечисление и настройки плагинов postCSS, которыми обрабатываются стилевые файлы
 let postCssPlugins = [
   autoprefixer({                                           // автопрефиксирование
-    browsers: ['last 2 version']
+    browsers: ['last 2 version'],
+    grid: true
   }),
   mqpacker({                                               // объединение медиавыражений с последующей их сортировкой
     sort: true
@@ -63,7 +65,7 @@ let jsList = [
   './node_modules/jquery/dist/jquery.min.js',
   './node_modules/jquery-migrate/dist/jquery-migrate.min.js',
   './node_modules/slick-carousel/slick/slick.js',
-  './node_modules/svg4everybody/dist/svg4everybody.js',
+  // './node_modules/svg4everybody/dist/svg4everybody.js',
   './node_modules/object-fit-images/dist/ofi.js',
   dirs.source + '/js/script.js',
 ];
@@ -117,6 +119,8 @@ gulp.task('copy:img', function () {
     return gulp.src(images)
       // .pipe(newer(dirs.build + '/img')) // потенциально опасно, к сожалению
       .pipe(rename({dirname: ''}))
+      // .pipe(gulp.dest(dirs.build + '/img'))
+      // .pipe(webp())
       .pipe(gulp.dest(dirs.build + '/img'));
   }
   else {
@@ -219,6 +223,7 @@ gulp.task('js', function () {
   if(jsList.length) {
     return gulp.src(jsList)
       .pipe(plumber({ errorHandler: onError }))             // не останавливаем автоматику при ошибках
+      .pipe(babel( {presets: ['env'] }))
       .pipe(concat('script.min.js'))                        // конкатенируем все файлы в один с указанным именем
       .pipe(uglify())                                       // сжимаем
       .pipe(gulp.dest(dirs.build + '/js'));                 // записываем
--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,84 @@
-svg4everybody(); // иницализация полифила для IE
+// svg4everybody(); // иницализация полифила для IE
 
 $(document).ready(function(){
   // весь ваш код c jQuery
-});
+
+  // SLIDER
+
+  $('.promo').slick({
+    dots: false,
+    infinite: true,
+    swipe: true,
+    autoplay: false,
+    speed: 500,
+    arrows: true,
+    adaptiveHeight: true
+  });
+
+  // BURGER
+
+  $('.mobile-nav').click(function(){
+    $('.menu__category').fadeToggle();
+  });
+
+
+
+
+
+
+
+
+})
 
 // Если на проекте нет jQuery, но хочется $( document ).ready... (IE9+)
-// function ready(fn) {
-//   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
-//     fn();
-//   } else {
-//     document.addEventListener('DOMContentLoaded', fn);
-//   }
-// }
-//
-// ready(function(){
-//   // code
-// });
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+ready(function(){
+
+  // Классы
+  const contentClass = 'books__content';
+  const listClass = 'books__list';
+  const itemClass = 'books__item';
+  const titleClass = 'books__title';
+  const priceClass = 'books__price';
+
+  const books = {
+    count: 256,
+    items: [
+      {
+        title: 'Правила мозга',
+        price: 700
+      },
+      {
+        title: 'Всегда вовремя',
+        price: 920
+      },
+      {
+        title: 'Супермен по привычке',
+        price: 590
+      },
+      {
+        title: 'Работа как внутренняя игра',
+        price: 700
+      },
+      {
+        title: 'Дзен-камера',
+        price: 840
+      },
+      {
+        title: 'Быть интровертом',
+        price: 680
+      }
+    ]
+  };
+
+
+
+
+});
